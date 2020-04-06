b39766903ca0d23be30f6015c58eb7e1df03d40e
Muhamad Hafiz <muhamadhfz@gmail.com>
2020-04-06 21:09:22 +0700

    creating utility function, adding increase-quantity functionality in cartReducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  //?(1) check if the added item already in the cart
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  ); //find gives us the first object that matched
+
+  //?(2) if it is, add 1 to the 'quantity' property
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  //?(3) if it isn't, spread the cartItems array, and give base 'quantity' property of 1 to the newly added item
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
